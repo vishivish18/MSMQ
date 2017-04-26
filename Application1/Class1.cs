29b56d0028da02be29700e9087e1f9146c2afda2
Vishal Ranjan <vishivish18@gmail.com>
2017-04-27 00:42:58 +0530

    basic msmq apps

---
+++ Application1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Messaging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter message to be sent");
+            Console.WriteLine("High priority messages should be sent with HP:");
+            string MessageToBeSend = Console.ReadLine();
+
+            //Get or Create queue
+            MessageQueue MyQueue;
+            if (MessageQueue.Exists(@".\Private$\MyQueue"))
+            {
+                MyQueue = new MessageQueue(@".\Private$\MyQueue");
+            }
+            else
+            {
+                MyQueue = MessageQueue.Create(@".\Private$\MyQueue");
+            }
+
+            //Create Message
+            Message MyMessage = new System.Messaging.Message();
+
+            //Set formatter for message
+            MyMessage.Formatter = new BinaryMessageFormatter();
+            MyMessage.Body = MessageToBeSend;
+            MyMessage.Label = "App1Message";
+
+            if (MessageToBeSend.Contains("HP:"))
+            {
+                MyMessage.Priority = MessagePriority.Highest;
+            }
+            else
+            {
+                MyMessage.Priority = MessagePriority.Normal;
+            }
+
+            MyQueue.Send(MyMessage);
+            Console.WriteLine("Thanks for sending message");
+            Console.ReadKey();
+
+
+
+
+            
+
+        }
+    }
+}
---
+++ Application2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Messaging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application2
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latest Message");
+
+            MessageQueue MyQueue;
+            MyQueue = new MessageQueue(@".\Private$\MyQueue");
+
+            MyQueue = new MessageQueue(@".\Private$\MyQueue");
+
+            Message MyMessage = MyQueue.Receive();
+            MyMessage.Formatter = new BinaryMessageFormatter();
+
+            Console.WriteLine(MyMessage.Body.ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
